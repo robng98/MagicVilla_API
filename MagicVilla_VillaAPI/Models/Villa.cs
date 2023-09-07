92d299917df448ac092163ae9279c2d21750e7f0
Roberto Nicácio <betonicacio10@hotmail.com>
2023-09-07 11:46:29 -0300

    Add further configs to EntityFramework + connection do new database

--- MagicVilla_VillaAPI/Models/Villa.cs
+++ MagicVilla_VillaAPI/Models/Villa.cs
@@ -1,9 +1,12 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace MagicVilla_VillaAPI.Models
 {
     public class Villa
     {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int Id { get; set; }
         [Required]
         [MaxLength(30)]
