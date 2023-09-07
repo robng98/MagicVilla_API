a0e71ebd7116eee852c0a45d91bb50eb33d84657
Roberto Nicácio <betonicacio10@hotmail.com>
2023-09-07 15:49:04 -0300

    Update correct types for VillaDTO + Delete VillaStore

--- MagicVilla_VillaAPI/Models/Dto/VillaDTO.cs
+++ MagicVilla_VillaAPI/Models/Dto/VillaDTO.cs
@@ -9,12 +9,11 @@ namespace MagicVilla_VillaAPI.Models.Dto
         [MaxLength(30)]
         public string Name { get; set; }
         public string Details { get; set; }
-        [Required]
-        public string Rate { get; set; }
-        public int Occupancy { get; set; }
+        public double Rate { get; set; }
         public int Sqft { get; set; }
-        public int ImageUrl { get; set; }
-        public int Amenity { get; set; }
+        public int Occupancy { get; set; }
+        public string ImageUrl { get; set; }
+        public string Amenity { get; set; }
 
     }
 }
